692351894dcfd03d9840c8a0b3b0182355ad45c1
RedwolfDark <30651692+RedwolfDark@users.noreply.github.com>
2025-10-10 09:00:11 +0100

    modifications2

--- front/src/utils/fetch.js
+++ front/src/utils/fetch.js
@@ -1,9 +1,7 @@
 const isDevelopment =
   process.env.NODE_ENV === "development" ||
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "192.168.1.119";
+  window.location.hostname === "localhost";
 
-// Utiliser l'IP locale au lieu de 127.0.0.1 pour permettre l'accès depuis mobile
 export const host = isDevelopment
-  ? "http://192.168.1.119:8000"
+  ? "http://127.0.0.1:8000"
   : "http://admin1.cpb-douala.com";
